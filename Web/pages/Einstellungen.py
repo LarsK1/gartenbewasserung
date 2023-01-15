6bd74215623813128125146e583740244fbfc8f3
Lars Kusch <lars@lars-kusch.de>
2023-01-15 17:06:01 +0100

    Hinzufügen von Standardwidgets

--- Web/pages/Einstellungen.py
+++ Web/pages/Einstellungen.py
@@ -2,22 +2,43 @@ import streamlit as st
 import yaml
 import streamlit_authenticator as stauth
 
+
+def auth():
+    if 'authenticator' not in st.session_state:
+        with open('../config.yaml') as file:
+            config = yaml.load(file, Loader=yaml.SafeLoader)
+        st.session_state["authenticator"] = stauth.Authenticate(
+            config['credentials'],
+            config['cookie']['name'],
+            config['cookie']['key'],
+            config['cookie']['expiry_days'],
+            config['preauthorized']
+        )
+    name, authentication_status, username = st.session_state["authenticator"].login('Anmelden', 'main')
+    st.session_state["authentication_status"] = authentication_status
+    st.session_state["authenticator"] = st.session_state["authenticator"]
+    if authentication_status == False:
+        st.error('Benutzername oder Passwort falsch')
+    elif authentication_status == None:
+        st.warning('Bitte gib deine Anmeldedaten ein')
+
+def sidebar():
+    st.sidebar.checkbox("Steuerung durch KI", False)
+
+def einstellungen():
+    datenbank = st.container()
+    datenbank.subheader("Datenbankeinstellungen")
+    datenbank.text_input("Host", help="URL des Datenbankservers")
+    datenbank.text_input("Benutzername")
+    datenbank.text_input("Passwort", type="password")
+    datenbank.text_input("Datenbank")
+
+
 st.set_page_config("Einstellungen - Gartensteuerung", "https://static.blumagine.de/blatt.png", menu_items={"About": "(c) Lars Kusch"})
 st.title('# Einstellungen')
-if 'authenticator' not in st.session_state:
-    with open('../config.yaml') as file:
-        config = yaml.load(file, Loader=yaml.SafeLoader)
-    st.session_state["authenticator"] = stauth.Authenticate(
-        config['credentials'],
-        config['cookie']['name'],
-        config['cookie']['key'],
-        config['cookie']['expiry_days'],
-        config['preauthorized']
-    )
-st.session_state["authenticator"].login('Anmelden', 'main')
+auth()
 if st.session_state["authentication_status"]:
+    einstellungen()
+    sidebar()
     st.session_state["authenticator"].logout('Abmelden', 'sidebar')
-elif st.session_state["authentication_status"] == False:
-    st.error('Benutzername oder Passwort falsch')
-elif st.session_state["authentication_status"] == None:
-    st.warning('Bitte gib deine Anmeldedaten ein')
+
--- Web/pages/Geräte.py
+++ Web/pages/Geräte.py
@@ -1,23 +1,52 @@
+import pandas
 import streamlit as st
 import yaml
 import streamlit_authenticator as stauth
+
+
+def auth():
+    if 'authenticator' not in st.session_state:
+        with open('../config.yaml') as file:
+            config = yaml.load(file, Loader=yaml.SafeLoader)
+        st.session_state["authenticator"] = stauth.Authenticate(
+            config['credentials'],
+            config['cookie']['name'],
+            config['cookie']['key'],
+            config['cookie']['expiry_days'],
+            config['preauthorized']
+        )
+    name, authentication_status, username = st.session_state["authenticator"].login('Anmelden', 'main')
+    st.session_state["authentication_status"] = authentication_status
+    st.session_state["authenticator"] = st.session_state["authenticator"]
+    if authentication_status == False:
+        st.error('Benutzername oder Passwort falsch')
+    elif authentication_status == None:
+        st.warning('Bitte gib deine Anmeldedaten ein')
+
+def sidebar():
+    st.sidebar.checkbox("Steuerung durch KI", False)
+
+def geraete():
+    st.subheader("Aktive Geräte")
+    st.table(pandas.DataFrame())
+    st.subheader("Neues Gerät hinzufügen")
+    if len(zonen) > 0:
+
+        with st.form("Neues Gerät", clear_on_submit=True):
+            typ = st.selectbox("Gerätetyp", ["Sensor", "Magnetventil"])
+            zone = st.multiselect("Zugehörige Zone", [1, 2, 3, 4, 5])
+            submitted = st.form_submit_button("anlegen")
+    else:
+        st.warning("Bitte legen Sie erst eine Zone an, bevor Sie Geräte anlegen können.")
+
+
+zonen = []
+
 st.set_page_config("Geräte - Gartensteuerung", "https://static.blumagine.de/blatt.png", menu_items={"About": "(c) Lars Kusch"})
 st.title('# Geräte')
-if 'authenticator' not in st.session_state:
-    with open('../config.yaml') as file:
-        config = yaml.load(file, Loader=yaml.SafeLoader)
-    st.session_state["authenticator"] = stauth.Authenticate(
-        config['credentials'],
-        config['cookie']['name'],
-        config['cookie']['key'],
-        config['cookie']['expiry_days'],
-        config['preauthorized']
-    )
-st.session_state["authenticator"].login('Anmelden', 'main')
+auth()
 if st.session_state["authentication_status"]:
+    geraete()
+    sidebar()
     st.session_state["authenticator"].logout('Abmelden', 'sidebar')
 
-elif st.session_state["authentication_status"] == False:
-    st.error('Benutzername oder Passwort falsch')
-elif st.session_state["authentication_status"] == None:
-    st.warning('Bitte gib deine Anmeldedaten ein')
