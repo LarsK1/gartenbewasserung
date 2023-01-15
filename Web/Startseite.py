903c9ae41b14784554f9379bcff9a1ba4eb83e57
Lars Kusch <lars@lars-kusch.de>
2023-01-15 16:47:00 +0100

    Hinzufügen von Standardwidgets

--- Web/Startseite.py
+++ Web/Startseite.py
@@ -1,29 +1,55 @@
 import streamlit as st
 import yaml
 import streamlit_authenticator as stauth
+import pandas
+
+def auth():
+    if 'authenticator' not in st.session_state:
+        with open('../config.yaml') as file:
+            config = yaml.load(file, Loader=yaml.SafeLoader)
+        st.session_state["authenticator"] = stauth.Authenticate(
+            config['credentials'],
+            config['cookie']['name'],
+            config['cookie']['key'],
+            config['cookie']['expiry_days'],
+            config['preauthorized']
+        )
+    name, authentication_status, username = st.session_state["authenticator"].login('Anmelden', 'main')
+    st.session_state["authentication_status"] = authentication_status
+    st.session_state["authenticator"] = st.session_state["authenticator"]
+    if authentication_status == False:
+        st.error('Benutzername oder Passwort falsch')
+    elif authentication_status == None:
+        st.warning('Bitte gib deine Anmeldedaten ein')
+
+
+def gesamt_widget():
+    reihe1, reihe2, reihe3 = st.columns(3)
+    reihe1.metric("Temperatur", "30°C", 2)
+    reihe2.metric("Niederschlag", "5mm/h", -1)
+    reihe3.metric("Ø Bodentemperatur", "12°C", "-5°C")
+
+
+def bewaesserungs_verlauf():
+    st.subheader("Letze Bewässerungsmenge")
+    st.bar_chart(pandas.read_csv(r"C:\Users\Lars-\Downloads\Mappe2.CSV", sep=";", ), x="Datum", y="Wassermenge")
+
+
+def geplannte_bewässerungen():
+    st.subheader("Nächste Bewässerung")
+    st.table(pandas.read_csv(r"C:\Users\Lars-\Downloads\Mappe3.CSV", sep=";", ))
+
+
+def sidebar():
+    st.sidebar.checkbox("Steuerung durch KI", False)
+
 
 st.set_page_config("Startseite - Gartensteuerung", "https://static.blumagine.de/blatt.png", menu_items={"About": "(c) Lars Kusch"})
-
-
 st.title('# Startseite')
-if 'authenticator' not in st.session_state:
-    with open('../config.yaml') as file:
-        config = yaml.load(file, Loader=yaml.SafeLoader)
-    st.session_state["authenticator"] = stauth.Authenticate(
-        config['credentials'],
-        config['cookie']['name'],
-        config['cookie']['key'],
-        config['cookie']['expiry_days'],
-        config['preauthorized']
-    )
-
-name, authentication_status, username = st.session_state["authenticator"].login('Anmelden', 'main')
-
-st.session_state["authenticator"] = st.session_state["authenticator"]
-if authentication_status:
+auth()
+if st.session_state["authentication_status"]:
+    sidebar()
+    gesamt_widget()
+    bewaesserungs_verlauf()
+    geplannte_bewässerungen()
     st.session_state["authenticator"].logout('Abmelden', 'sidebar')
-elif authentication_status == False:
-    st.error('Benutzername oder Passwort falsch')
-elif authentication_status == None:
-    st.warning('Bitte gib deine Anmeldedaten ein')
-
