1c4496471774f80fc25063e61247b943aab68a64
Lars Kusch <lars@lars-kusch.de>
2023-01-15 13:57:01 +0100

    Initales Setup

--- Web/Startseite.py
+++ Web/Startseite.py
@@ -2,7 +2,7 @@ import streamlit as st
 import yaml
 import streamlit_authenticator as stauth
 
-with open('../config.yml') as file:
+with open('../config.yaml') as file:
     config = yaml.load(file, Loader=yaml.SafeLoader)
 
 authenticator = stauth.Authenticate(
