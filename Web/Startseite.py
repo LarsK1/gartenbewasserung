f406374d111eb2ace384fa57abed9a89de6713f0
Lars Kusch <lars@lars-kusch.de>
2023-01-15 14:31:00 +0100

    Verbesserung der Anmeldefunktion

--- Web/Startseite.py
+++ Web/Startseite.py
@@ -4,23 +4,24 @@ import streamlit_authenticator as stauth
 
 st.set_page_config("Startseite - Gartensteuerung", "https://static.blumagine.de/blatt.png", menu_items={"About": "(c) Lars Kusch"})
 
-with open('../config.yaml') as file:
-    config = yaml.load(file, Loader=yaml.SafeLoader)
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
+st.title('# Startseite')
+if 'authenticator' not in st.session_state:
+    with open('../config.yaml') as file:
+        config = yaml.load(file, Loader=yaml.SafeLoader)
+    st.session_state["authenticator"] = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
 
-name, authentication_status, username = authenticator.login('Anmelden', 'main')
+name, authentication_status, username = st.session_state["authenticator"].login('Anmelden', 'main')
 
-st.session_state["authenticator"] = authenticator
+st.session_state["authenticator"] = st.session_state["authenticator"]
 if authentication_status:
-    authenticator.logout('Abmelden', 'sidebar')
-    st.title('# Startseite')
+    st.session_state["authenticator"].logout('Abmelden', 'sidebar')
 elif authentication_status == False:
     st.error('Benutzername oder Passwort falsch')
 elif authentication_status == None:
--- Web/pages/Geräte.py
+++ Web/pages/Geräte.py
@@ -1,9 +1,22 @@
 import streamlit as st
+import yaml
+import streamlit_authenticator as stauth
 st.set_page_config("Geräte - Gartensteuerung", "https://static.blumagine.de/blatt.png", menu_items={"About": "(c) Lars Kusch"})
-
+st.title('# Geräte')
+if 'authenticator' not in st.session_state:
+    with open('../config.yaml') as file:
+        config = yaml.load(file, Loader=yaml.SafeLoader)
+    st.session_state["authenticator"] = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
+st.session_state["authenticator"].login('Anmelden', 'main')
 if st.session_state["authentication_status"]:
     st.session_state["authenticator"].logout('Abmelden', 'sidebar')
-    st.title('Geräte')
+
 elif st.session_state["authentication_status"] == False:
     st.error('Benutzername oder Passwort falsch')
 elif st.session_state["authentication_status"] == None:
